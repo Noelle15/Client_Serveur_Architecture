41a5d82fadc264fa5ec28212c5f686b700c57ce7
rabearisoa <leiya.rabe@gmail.com>
2020-12-16 10:56:55 +0100

    Ajout d'une nouvelle branche avec taille donnees

--- Main.c
+++ Main.c
@@ -4,13 +4,14 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
+#include <string.h>
 
 //Prototypes
-int addition(int x1, int x2);
-int soustraction(int x1, int x2);
-int multiplication(int x1, int x2);
-int puissance(int x1, int x2);
-
+int addition(char * donnees);
+int soustraction(char * donnees);
+int multiplication(char * donnees);
+int puissance(char * donnees);
 
 int main()
 {
@@ -31,18 +32,21 @@ int main()
 	//creation des pipe
 	int p1[2];
 	int p2[2];
-	int p3[2];
+	int p3[2];//Pipe qui envoie le choix de l'opération au serveur
+	int p4[4];//pipe pour envoyer la taille des donnees client(fils) au serveur(pere)	
 
 	pipe(p1);
 	pipe(p2);
-	pipe(p3); //Pipe qui envoie le choix de l'opération au serveur	
-
+	pipe(p3); 
+	pipe(p4);
 
 	//variable identification processus
 	pid_t pcs;
 
 	//variables pour les fonctions de calculs
-	int valEntrees[2];
+	char * donnees; //Taille des données entrées par l'utilisateur
+	int tailleDonnees; //strlen(donnees)
+	int tailleDonneesLuServer;
 	int resultat;
 	int produit;
 	int choixOpe;
@@ -55,66 +59,73 @@ int main()
 	pcs = fork();
 
 	if (pcs == 0){
-		//demande les chiffres à additionner
-		do{
-			printf("Veuillez choisir votre opération; [addition -> 1 : soustraction -> 2 : multiplication -> 3 : puissance -> 4 \n");
-			scanf("%d",&choixOpe);
-		}while(choixOpe > 4 || choixOpe < 1);
-		
+			//demande les chiffres à additionner
+			do{
+				printf("Veuillez choisir votre opération; [addition -> 1 : soustraction -> 2 : multiplication -> 3 : puissance -> 4 : distance -> 5\n");
+				scanf("%d",&choixOpe);
+			}while(choixOpe > 5 || choixOpe < 1);
 
-		printf("Veuillez indiquer le 1er entier\n");
-		scanf("%d",&valEntrees[0]);
-		printf("Veuillez indiquer le 2nd entier\n");
-		scanf("%d",&valEntrees[1]);
+			
+			printf("Veuillez les valeurs à calculer separees de ;\n");
+			scanf("%s",donnees); //5;4;4;6;9
+			tailleDonnees = strlen(donnees);
+				
+			//taille des données: sizeof(char)
 
-			//------ Addition ---------//
-		//le client est le pcs qui écrit dans le tube
-		close(p1[0]); // vérouille la lecture du père 
-		close(p2[1]); //fermer l'ecriture sur le pipe du père
-		close(p3[0]);//fermer la lecture du pipe sur le choix de l'operation 
+			//le client est le pcs qui écrit dans le tube
+			close(p1[0]); // vérouille la lecture du père 
+			close(p2[1]); //fermer l'ecriture sur le pipe du père
+			close(p3[0]);//fermer la lecture du pipe sur le choix de l'operation 
+			close(p4[0]);//fermer la lecture du pipe sur la taille des données
 
-		write(p3[1],&choixOpe,sizeof(int));
-		write(p1[1],valEntrees,2*sizeof(int));//envoyer au serveur
-		
-		read(p2[0],&resultat,sizeof(int));
-		printf(" le resultat renvoyé par le serveur: %d\n",resultat);	
-		exit(0);
+			write(p3[1],&choixOpe,sizeof(int));
+			write(p4[1],&tailleDonnees,sizeof(int));//envoyer la taille des donnees au serveur
+			write(p1[1],&donnees,tailleDonnees);//envoyer les donnees brutes au serveur
+			
+			read(p2[0],&resultat,sizeof(int));
+			printf(" le resultat renvoyé par le serveur: %d\n",resultat);
 
-	}
+			exit(0);
+		}
+
 	else{
 		//le serveur lit le tube et répond 
+		printf("Vous êtes maintenant dans le serveur" );
+
 		close(p1[1]);//femer l'écriture du client
 		close(p2[0]);//fermer la lecture du client
 		close(p3[1]);//fermer l'ecriture dans le pipe du choix de l'operation 
+		close(p4[1]);//fermer l'ecriture dans le pipe de la taille des donnees
 
 		read(p3[0],&choixOpe,sizeof(int));
-		read(p1[0],valEntrees,2*sizeof(int));
+		read(p4[0],&tailleDonneesLuServer,sizeof(int));// on lit la taille des données
+		read(p1[0],&donnees,tailleDonneesLuServer);//on lit ce qui se trouve dans le tube p1 et on le met dans donnees avec une taille de tailleDonneesLuServer
 
 		switch(choixOpe){
 			case 1 : 
-				resultat = addition(valEntrees[0],valEntrees[1]);
+				resultat = addition(donnees);
 				printf("Le resultat: %d\n",resultat );
 				ope = '+';
 			break;
 			case 2 : 
-				resultat = soustraction(valEntrees[0],valEntrees[1]);
+				resultat = soustraction(donnees);
 				printf("Le resultat: %d\n",resultat );
 				ope = '-';
 			break;
 			case 3 : 
-				resultat = multiplication(valEntrees[0],valEntrees[1]);
+				resultat = multiplication(donnees);
 				printf("Le resultat: %d\n",resultat );
 				ope = '*';
 			break;
 			case 4 : 
-				resultat = puissance(valEntrees[0],valEntrees[1]);
+				resultat = puissance(donnees);
 				printf("Le resultat: %d\n",resultat );
 				ope = '^';
 			break; 
 
 		}
 
-		printf(" le client a dit: %d %c %d\n",valEntrees[0],ope,valEntrees[1]);
+		printf(" le client a dit: %s",donnees);
 		write(p2[1],&resultat,sizeof(int));
 		close(p2[1]);//fermer l'écriture
 		wait(0);
@@ -123,22 +134,22 @@ int main()
 	return 0;
 }
 
-int addition(int x1, int x2){
-	printf("Le resultat: %d\n",(x1 + x2) );
-	return x1 + x2;
+int addition(char * donnees){
+	printf("Les donnees envoyees par le client: %s\n",donnees );
+	return 1;
 }
 
-int soustraction(int x1, int x2){
-	printf("Le resultat: %d\n",(x1 - x2) );
-	return x1 - x2;
+int soustraction(char * donnees){
+	printf("Les donnees envoyees par le client: %s\n",donnees );
+	return 1;
 }
 
-int multiplication(int x1, int x2){
-	printf("Le resultat: %d\n",(x1 * x2) );
-	return x1 * x2;
+int multiplication(char * donnees){
+	printf("Les donnees envoyees par le client: %s\n",donnees );
+	return 1;
 }
 
-int puissance(int x1, int x2){
-	printf("Le resultat: %f\n",pow(x1,x2));
-	return (int)pow(x1,x2);
+int puissance(char * donnees){
+	printf("Les donnees envoyees par le client: %s\n",donnees );
+	return 1;
 }
