bd048bb2a0e2124dcf374e2d41ede78e38684510
Noelle15 <56430898+Noelle15@users.noreply.github.com>
2020-12-17 01:39:29 +0100

    dist + loop ok avec qql ajustements nécessaires

--- Main.c
+++ Main.c
@@ -13,23 +13,20 @@ int soustraction(char * donnees);
 int multiplication(char * donnees);
 int puissance(char * donnees);
 int distance(char * donnees);
+int continuer();
 
 //Le séparateur des données reçues est ';'
 const char separators [1] = ";";
-const char separatorDistance[1] = ",";
 
 int main()
 {
+	
+	printf("another loop");
 	//creation des pipe
 	int p1[2];//Pipe client -> serveur
 	int p2[2];//Pipe client <- serveur
 	int p3[2];//Pipe qui envoie le choix de l'opération au serveur
 	int p4[4];//pipe pour envoyer la taille des donnees client(fils) au serveur(pere)	
-
-	pipe(p1);
-	pipe(p2);
-	pipe(p3); 
-	pipe(p4);
 
 	//variable d'identification processus
 	pid_t pcs;
@@ -41,84 +38,90 @@ int main()
 	int resultat;
 	int produit;
 	int choixOpe;
-
-	//creation du processus client
-	pcs = fork();
-
-	if (pcs == 0){ //client
-			do{
-				printf("Veuillez choisir votre opération; [addition -> 1 : soustraction -> 2 : multiplication -> 3 : puissance -> 4 : distance -> 5\n");
-				scanf("%d",&choixOpe);
-			}while(choixOpe > 5 || choixOpe < 1);
-			
-			printf("Veuillez les valeurs à calculer separees de ;\n");
-			if(choixOpe = 5)
-				printf("l'ordre des valeurs : xA;yA;xB;yB (les valeurs en plus seront ignorées)\n");
-			scanf("%s",donnees); //5;4;4;6;9
-			
-			printf("Les données entrées: %s\n",donnees);
-			tailleDonnees = strlen(donnees);
-			printf("La taille des données entrées: %d\n",tailleDonnees);
-			
-			//le client est le pcs qui écrit dans le tube
-			close(p4[0]);//fermer la lecture du pipe sur la taille des données
-			close(p1[0]); // vérouille la lecture du père 
-			close(p2[1]); //fermer l'ecriture sur le pipe du père
-			close(p3[0]);//fermer la lecture du pipe sur le choix de l'operation 
-			
-			printf("envoie le choix de l'operation au serveur");
-			write(p3[1],&choixOpe,sizeof(int));
-			printf("envoyer la taille des donnees au serveur");
-			write(p4[1],&tailleDonnees,sizeof(int));//envoyer la taille des donnees au serveur
-			write(p1[1],donnees,tailleDonnees);//envoyer les donnees brutes au serveur
-			
-			read(p2[0],&resultat,sizeof(int));
-			printf(" le resultat renvoyé par le serveur: %d\n",resultat);
-
-			exit(0);
-		}
-
-	else{
-		//le serveur lit le tube et répond 
-		close(p4[1]);//fermer l'ecriture dans le pipe de la taille des donnees
-		close(p1[1]);//femer l'écriture du client
-		close(p2[0]);//fermer la lecture du client
-		close(p3[1]);//fermer l'ecriture dans le pipe du choix de l'operation 
+	
+	do{
 		
-		read(p3[0],&choixOpe,sizeof(int));
-		read(p4[0],&tailleDonneesLuServer,sizeof(int));// on lit la taille des données
-		read(p1[0],&donnees,tailleDonneesLuServer);//on lit ce qui se trouve dans le tube p1 et on le met dans donnees avec une taille de tailleDonneesLuServer
+		pipe(p1);
+		pipe(p2);
+		pipe(p3); 
+		pipe(p4);
+		
+		//creation du processus client
+		pcs = fork();
+		
+		if (pcs == 0){ //client
+				do{
+					//GERER LE CAS oU oN TAPE UN MAUVAIS CHOIX COMME 1;
+					printf("Veuillez choisir votre opération; [addition -> 1 : soustraction -> 2 : multiplication -> 3 : puissance -> 4 : distance -> 5\n");
+					scanf(" %d",&choixOpe);
+				}while(choixOpe > 5 || choixOpe < 1);
+				
+				printf("Veuillez les valeurs à calculer separees de ;\n");
+				//GERER ERREUR POUR DISTANCE ET REGARdER RETOUR DONNEES
+				if(choixOpe == 5){
+					printf("l'ordre des valeurs : xA;yA;xB;yB (les valeurs en plus seront ignorées)\n");
+				}
+				scanf(" %s",donnees); //5;4;4;6;9
+				
+				printf("Les données entrées: %s\n",donnees);
+				tailleDonnees = strlen(donnees);
+				printf("La taille des données entrées: %d\n",tailleDonnees);
+				
+				//le client est le pcs qui écrit dans le tube
+				close(p4[0]);//fermer la lecture du pipe sur la taille des données
+				close(p1[0]); // vérouille la lecture du père 
+				close(p2[1]); //fermer l'ecriture sur le pipe du père
+				close(p3[0]);//fermer la lecture du pipe sur le choix de l'operation 
+				
+				write(p3[1],&choixOpe,sizeof(int));
+				write(p4[1],&tailleDonnees,sizeof(int));//envoyer la taille des donnees au serveur
+				write(p1[1],donnees,tailleDonnees);//envoyer les donnees brutes au serveur
+				
+				read(p2[0],&resultat,sizeof(int));
 
-		switch(choixOpe){
-			case 1 : 
-				printf("partie addition");
-				resultat = addition(donnees);
-				printf("Le resultat: %d\n",resultat );
-			break;
-			case 2 : 
-				resultat = soustraction(donnees);
-				printf("Le resultat: %d\n",resultat );
-			break;
-			case 3 : 
-				resultat = multiplication(donnees);
-				printf("Le resultat: %d\n",resultat );
-			break;
-			case 4 : 
-				resultat = puissance(donnees);
-				printf("Le resultat: %d\n",resultat );
-			break; 
-			case 5 :
-				resultat = distance(donnees);
-				printf("Le resultat : %d\n", resultat);
-			break;
-
+				exit(0);
 		}
 
-		printf(" le server confirme que le client a dit: %s",donnees);
-		write(p2[1],&resultat,sizeof(int));
-		close(p2[1]);//fermer l'écriture
-		wait(0);
-	}
+		else{
+			//le serveur lit le tube et répond 
+			close(p4[1]);//fermer l'ecriture dans le pipe de la taille des donnees
+			close(p1[1]);//femer l'écriture du client
+			close(p2[0]);//fermer la lecture du client
+			close(p3[1]);//fermer l'ecriture dans le pipe du choix de l'operation 
+			
+			read(p3[0],&choixOpe,sizeof(int));
+			read(p4[0],&tailleDonneesLuServer,sizeof(int));// on lit la taille des données
+			read(p1[0],&donnees,tailleDonneesLuServer);//on lit ce qui se trouve dans le tube p1 et on le met dans donnees avec une taille de tailleDonneesLuServer
+
+			switch(choixOpe){
+				case 1 : 
+					resultat = addition(donnees);
+					printf("Le resultat: %d\n",resultat );
+				break;
+				case 2 : 
+					resultat = soustraction(donnees);
+					printf("Le resultat: %d\n",resultat );
+				break;
+				case 3 : 
+					resultat = multiplication(donnees);
+					printf("Le resultat: %d\n",resultat );
+				break;
+				case 4 : 
+					resultat = puissance(donnees);
+					printf("Le resultat: %d\n",resultat );
+				break; 
+				case 5 :
+					resultat = distance(donnees);
+					printf("Le resultat : %d\n", resultat);
+				break;
+
+			}
+
+			write(p2[1],&resultat,sizeof(int));
+			close(p2[1]);//fermer l'écriture
+			wait(0);
+		}
+	}while(continuer() == 0);
 
 	return 0;
 }
@@ -196,4 +199,17 @@ int distance(char * donnees){
 	
     printf("DISTANCE: %d\n", res );
 	return res;
+}
+
+int continuer(){
+	char continuer = '\0';
+	do{
+		printf("Voulez-vous continuer ? (O/N)\n");
+		scanf(" %c",&continuer);
+		continuer = (toupper(continuer));
+	} while (continuer != 'O' && continuer !='N'); //continuer ne doit pas être vide
+	
+	if(continuer == 'N')
+		return 1;
+	return 0;
 }
